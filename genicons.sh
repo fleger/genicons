0b8f6f79c2157004936cbfe57cb2ab29d7caf51e
Florian Léger <florian.leger6@gmail.com>
2018-11-20 09:19:10 +0100

    Tab -> Space

--- genicons.sh
+++ genicons.sh
@@ -7,14 +7,14 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 if [[ $# != 3 ]]; then
-	echo "Usage: $0 iconsource basedir iconpath.{png|svg}"
-	echo
-	echo "Note: iconsource must be an image with the same height and width"
-	echo
-	echo "Example:"
-	echo "Generate application icons derived from myapp.png for the hicolor iconset:"
-	echo "$0 myapp.png /usr/share/icons/hicolor apps/myapp.png"
-	exit 1
+    echo "Usage: $0 iconsource basedir iconpath.{png|svg}"
+    echo
+    echo "Note: iconsource must be an image with the same height and width"
+    echo
+    echo "Example:"
+    echo "Generate application icons derived from myapp.png for the hicolor iconset:"
+    echo "$0 myapp.png /usr/share/icons/hicolor apps/myapp.png"
+    exit 1
 fi
 
 w=$(convert "$1" -print "%w" /dev/null)
@@ -22,28 +22,28 @@ h=$(convert "$1" -print "%h" /dev/null)
 fmt=$(convert "$1" -print "%m" /dev/null)
 
 if [[ "$h" != "$w" ]]; then
-	echo "Only square icons are supported!"
-	exit 2
+    echo "Only square icons are supported!"
+    exit 2
 fi
 
 readonly -a SIZES=(512 384 256 192 128 96 72 64 48 36 32 24 22 16)
 
 for s in ${SIZES[@]}; do
-	if [[ "$fmt" != "SVG" ]] &&  [[ "$w" -lt "$s" ]]; then
-		continue
-	fi
-	target_file="$2/${s}x${s}/${3}"
-	echo "Generating $target_file"
-	install -dm755 "$(dirname "$target_file")"
-	if [[ "$fmt" != "SVG" ]]; then
-		args=()
-		if [[ "$w" != "$s" ]]; then
-			args=(-resize ${s}x${s}\> -unsharp 1.5x1+0.7+0.02)
-		fi
-		convert "$1" "${args[@]}" "$target_file"
-	else
-		# Rasterize SVG
-		rsvg-convert -w "$s" -h "$s" -f png -o "$target_file" "$1"
-	fi
-	optipng -o7 "$target_file"
+    if [[ "$fmt" != "SVG" ]] &&  [[ "$w" -lt "$s" ]]; then
+        continue
+    fi
+    target_file="$2/${s}x${s}/${3}"
+    echo "Generating $target_file"
+    install -dm755 "$(dirname "$target_file")"
+    if [[ "$fmt" != "SVG" ]]; then
+        args=()
+        if [[ "$w" != "$s" ]]; then
+            args=(-resize ${s}x${s}\> -unsharp 1.5x1+0.7+0.02)
+        fi
+        convert "$1" "${args[@]}" "$target_file"
+    else
+        # Rasterize SVG
+        rsvg-convert -w "$s" -h "$s" -f png -o "$target_file" "$1"
+    fi
+    optipng -o7 "$target_file"
 done
